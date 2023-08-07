7c4382e4d1f8918a25396eff79c172af8770e14d
n4p5t3r <dev@rrsolucionesit.com>
2023-08-07 13:05:04 -0500

    update

--- src/clics.py
+++ src/clics.py
@@ -1,11 +1,13 @@
+# Web-Scrap - Clics Automaticos
+# Desarrollado por Ricardo Rosero - n4p5t3r
+# Email: rrosero2000@gmail.com
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 # Configurar el navegador
 options = webdriver.FirefoxOptions()
+options.add_argument('--headless')  # Habilitar el modo headless, para que se ejecute via terminal
 
 # Inicializar el navegador Firefox
 driver = webdriver.Firefox(options=options)
@@ -14,24 +16,22 @@ driver = webdriver.Firefox(options=options)
 url = 'https://www.dian.gov.co/dian/ventasremates/Paginas/Donaciones.aspx'
 driver.get(url)
 
-# Esperar a que se cargue la página
-wait = WebDriverWait(driver, 10)
-wait.until(EC.presence_of_element_located((By.ID, 'collapse4')))  
+# Realizar clic en todos los elementos con href="#collapse4" utilizando JavaScript
+links = driver.find_elements(By.CSS_SELECTOR, '[href="#collapse4"]')
+for link in links:
+    driver.execute_script("arguments[0].click();", link)
 
-for link in driver.find_elements(By.CSS_SELECTOR, '[href="#collapse4"]'):
-    link.click()
+# Realizar clic en todos los elementos con la referencia '/_layouts/15/images/spcommon.png?rev=40' utilizando JavaScript
+elements = driver.find_elements(By.CSS_SELECTOR, '[src*="/_layouts/15/images/spcommon.png?rev=40"]')
+for element in elements:
+    driver.execute_script("arguments[0].click();", element)
 
 # Obtener el contenido HTML de la página
 page_content = driver.page_source
 
 # Guardar el contenido en un archivo HTML
-with open('pagina_web.html', 'w', encoding='utf-8') as file:
+with open('./src/pagina_web.html', 'w+', encoding='utf-8') as file:
     file.write(page_content)
-
-driver.execute_script("javascript:ExpCollGroup('collapse4', 'img_collapse4',event, false);return false;")
-driver.execute_script("javascript:ExpCollGroup('collapse5', 'img_collapse5',event, false);return false;")
-driver.execute_script("javascript:ExpCollGroup('scriptWPQ5', 'img_scriptWPQ5',event, false);return false;")
-
 
 # Finalizar la sesión del navegador
 driver.quit()
